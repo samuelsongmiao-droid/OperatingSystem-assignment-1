126257cc1ff684c3557c96cefd812a509bfee4dc
samuelsongmiao-droid <samuelsongmiao@gmail.com>
2026-05-25 09:34:20 -0800

    Implemented C core simulator

--- C_core/main_sim.c
+++ C_core/main_sim.c
@@ -5,12 +5,12 @@ int main()
     printf("===== EDUOS SIMULATION =====\n");
 
     run_process_manager();
-    /*run_thread_manager();
+    run_thread_manager();
     run_thread_manager_race();
     run_thread_manager_fixed();
     run_thread_pool();
     run_ipc_module();
-*/
+
     printf("\n===== Simulation Finished =====\n");
     return 0;
 }
